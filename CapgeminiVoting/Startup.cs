df77a204b883bf74899ea3fa311b9513c4019150
matthiasverhelst <matthiasverhelst@hotmail.com>
2014-08-21 10:16:05 +0100

    First commit

---
+++ CapgeminiVoting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CapgeminiVoting.Startup))]
+namespace CapgeminiVoting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
