8a9e2c15e830494dadcbdfabff803e0cd5b44bda
theyman <tom_heyman@hotmail.com>
2014-09-11 20:54:37 +0200

    First attempt on dummy data (to be continued)

--- CapgeminiVoting/Global.asax.cs
+++ CapgeminiVoting/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CapgeminiVoting.Contexts;
+using CapgeminiVoting.DTO;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,10 +15,46 @@ namespace CapgeminiVoting
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<VotingContext>(new DbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        private class DbInitializer : DropCreateDatabaseAlways<VotingContext>
+        {
+            protected override void Seed(VotingContext context)
+            {
+                var events = new List<DTOEvent>
+                {
+                    new DTOEvent       {id = 1,
+                                        code = 69,
+                                        name = "Sprint 1 Questionaire",
+                                        startDate = DateTime.Now,
+                                        endDate = DateTime.Now.AddDays(10),
+                                        description = "Bla.",
+                                        userName = "Tom"
+                    }
+                };
+
+                events.ForEach(p => context.events.Add(p));
+
+                var questions = new List<DTOQuestion>
+                {
+                    new DTOQuestion {   id = 1,
+                                        question = "Was de pizza te pikant?",
+                                        questionType = 0,
+                                        eventId = 1
+                    }
+                };
+
+                questions.ForEach(p => context.questions.Add(p));
+
+
+                context.SaveChanges();
+            }
+        }
+
     }
 }
