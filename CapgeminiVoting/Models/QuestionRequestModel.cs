38aa3aa539da1ebaefc8f81dc5d7c7f3962bb168
theyman <tom_heyman@hotmail.com>
2014-10-29 20:52:50 +0100

    Missed cs

---
+++ CapgeminiVoting/Models/QuestionRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CapgeminiVoting.Models
+{
+    public class QuestionRequestModel
+    {
+        [Required(ErrorMessage = "")]
+        public string EventCode { get; set; }
+
+        public int QuestionNumber { get; set; }
+    }
+}
